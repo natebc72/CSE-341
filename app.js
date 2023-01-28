fad7af70373176aaa75c4a5a033d283e28c15e96
natebc72 <nbchristiansen72@gmail.com>
2023-01-27 19:11:27 -0800

    stretch activity

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
     next();
   })
   .use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
