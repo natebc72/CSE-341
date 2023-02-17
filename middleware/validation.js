7987972519ea6801d328d21c13a61f11bd21e907
natebc72 <nbchristiansen72@gmail.com>
2023-02-16 19:16:06 -0800

    updates

--- middleware/validation.js
+++ middleware/validation.js
@@ -3,11 +3,11 @@ const middleware = require('../help/validation');
 const saveBook = (req, res, next) => {
   const validationRule = {
     ispn: 'required|string',
-    title: 'required|string',
-    author: 'required|string',
+    title: 'required|string|min:10',
+    author: 'required|string|min:10',
     release: 'required|string',
     purchased: 'required|string',
-    review: 'required|string',
+    review: 'required|string|min:20',
     rating: 'required|string'
   };
   middleware(req.body, validationRule, {}, (err, status) => {
